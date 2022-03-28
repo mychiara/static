af37f6a89e03813d6643dd29910bc4b75683af38
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-28 17:42:18 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -7,10 +7,17 @@
          <div><a id='close-floatads' onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/></a></div>
     <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
 <!--Script iklan-->
-<script async="async" data-cfasync="false" src="//pl16176169.safestgatetocontent.com/1cdcac7d654c1999aa7295ebea633aff/invoke.js"></script>
-<div id="container-1cdcac7d654c1999aa7295ebea633aff"></div>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'fc8c284434e227dfa520ab45f6931c94',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
+</script>
 <!--Akhir script iklan-->
         </div>
  </div><!--end: floating ads-->
-<script type='text/javascript' src='//pl16176151.safestgatetocontent.com/27/5b/4a/275b4a656539eaa4e4cdc0cd04947f16.js'></script>
 //]]>
