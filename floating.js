822f8dc6d6ce55eec10986632a8bcac593142ba8
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-28 17:25:24 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,21 +1,21 @@
-<script type='text/javascript'>
-        $(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#btm_banner&#39;).hide(90);});});
-</script>
-        <!--start: floating ads-->
-        <div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
-         <div><a id='close-floatads' onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/></a></div>
-    <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
-<!--Script iklan-->
-<script type="text/javascript">
+&lt;script type=&#039;text/javascript&#039;&gt;
+        $(document).ready(function() {$(&amp;#39;img#closed&amp;#39;).click(function(){$(&amp;#39;#btm_banner&amp;#39;).hide(90);});});
+&lt;/script&gt;
+        &lt;!--start: floating ads--&gt;
+        &lt;div id=&#039;floatads&#039; style=&#039;width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999&#039;&gt;
+         &lt;div&gt;&lt;a id=&#039;close-floatads&#039; onclick=&#039;document.getElementById(&amp;apos;floatads&amp;apos;).style.display = &amp;apos;none&amp;apos;;&#039; style=&#039;cursor:pointer;&#039;&gt;&lt;img alt=&#039;close&#039; src=&#039;https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif&#039; title=&#039;close button&#039;/&gt;&lt;/a&gt;&lt;/div&gt;
+    &lt;div style=&#039;text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto&#039;&gt;
+&lt;!--Script iklan--&gt;
+&lt;script type=&quot;text/javascript&quot;&gt;
 	atOptions = {
-		'key' : '0c36e62040e13f23ed831c26d97931ec',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
+		&#039;key&#039; : &#039;0c36e62040e13f23ed831c26d97931ec&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/0c36e62040e13f23ed831c26d97931ec/invoke.js"></scr' + 'ipt>');
-</script>
-<!--Akhir script iklan-->
-        </div>
- </div><!--end: floating ads-->
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.effectivedisplayformats.com/0c36e62040e13f23ed831c26d97931ec/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
+&lt;/script&gt;
+&lt;!--Akhir script iklan--&gt;
+        &lt;/div&gt;
+ &lt;/div&gt;&lt;!--end: floating ads--&gt;
