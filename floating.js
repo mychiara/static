16979ccbfce199401f49ed710a9e2dda4bea11de
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-28 17:37:54 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,1 +1,14 @@
-&lt;script type=&#039;text/javascript&#039; src=&#039;//pl16176151.safestgatetocontent.com/27/5b/4a/275b4a656539eaa4e4cdc0cd04947f16.js&#039;&gt;&lt;/script&gt;
+//<![CDATA[
+<script type='text/javascript'>
+        $(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#btm_banner&#39;).hide(90);});});
+</script>
+        <!--start: floating ads-->
+        <div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
+         <div><a id='close-floatads' onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/></a></div>
+    <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
+<!--Script iklan-->
+
+<!--Akhir script iklan-->
+        </div>
+ </div><!--end: floating ads-->
+//]]>
