e698d3d763d75e1dc18479c8a16fee43cd5515aa
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-28 17:54:55 +0800

    Update floating.js

--- floating.js
+++ floating.js
@@ -1,30 +1,11 @@
-<!-- floating ads https://sumba.club -->
-$(document).ready(function() {
-$('img#closed').click(function(){
-$('#btm_banner').hide(90);
-});
-});
+type='text/javascript'>
+        $(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#btm_banner&#39;).hide(90);});});
 </script>
-<style type='text/css'>
-div#btm_banner {
-bottom: 0;
-position: fixed;
-width: 100%;
-opacity: 0.9;
-left: 0;
-}
-div#btm_banner img{
-border:0;
-cursor:pointer;
-}
-</style>
-<div style='height: 0px;'></div>
-<div align='center' id='btm_banner' style='height: 120px; z-index: 9999;'>
-<div style='text-align: right; width: 728px; height: 10px;'>
-<img id='closed' src='http://4.bp.blogspot.com/-9MWyoN5VsJM/TivTpPyUuhI/AAAAAAAABL0/ldO739MTRBg/s1600/close3.png'/></div>
-<div style='clear: both;'></div>
-<p><p><center>
-
+        <!--start: floating ads-->
+        <div id='floatads' style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
+         <div><a id='close-floatads' onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;' style='cursor:pointer;'><img alt='close' src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif' title='close button'/></a></div>
+    <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
+        <!--Script iklan-->
 <script type="text/javascript">
 	atOptions = {
 		'key' : 'fc8c284434e227dfa520ab45f6931c94',
@@ -35,6 +16,6 @@ cursor:pointer;
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/fc8c284434e227dfa520ab45f6931c94/invoke.js"></scr' + 'ipt>');
 </script>
-</center></p>
-</p>
-</div> 
+<!--Akhir script iklan-->
+        </div>
+ </div><!--end: floating ads-->
